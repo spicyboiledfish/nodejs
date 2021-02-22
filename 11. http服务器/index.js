164248695a15f326465599fb3f062fde1aa44ae7
wangrui <wangrui@hupu.com>
2021-02-22 09:57:46 +0800

    feat: 更新

--- 11. http服务器/index.js
+++ 11. http服务器/index.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+const querystring = require('querystring');
 
 http.createServer(function(req, res) {
+    console.log(url.parse(req.url));
     if (req.url === '/favicon.ico') {
         res.writeHead(200);
         res.end();
