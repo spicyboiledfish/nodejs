6b1794bb6b9380b2efd2009108b0816de1ff5293
wangrui <wangrui@hupu.com>
2021-02-24 10:37:37 +0800

    feat: buffer

---
+++ 12.buffer编解码二进制/index.js
@@ -0,0 +1,10 @@
+const buffer1 = Buffer.from('geekbang');
+const buffer2 = Buffer.from([1, 2, 3, 4]);
+const buffer3 = Buffer.alloc(20);
+
+console.log('buffer1', buffer1); // <Buffer 67 65 65 6b 62 61 6e 67>
+console.log('buffer2', buffer2); // <Buffer 01 02 03 04>
+console.log('buffer3', buffer3); // <Buffer 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00>
+
+buffer2.writeInt8(12, 0);
+console.log(buffer2);
