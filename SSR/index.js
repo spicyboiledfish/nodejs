bff95a3f4034b33c0550ef76b0c21491b24b59af
wangrui <wangrui@hupu.com>
2021-03-12 11:37:48 +0800

    feat: 简单的直出html页面

---
+++ SSR/index.js
@@ -0,0 +1,40 @@
+const React = require('react');
+const http = require('http');
+const { renderToString } = require('react-dom/server');
+
+class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+
+    render() {
+        return (
+            <div>Hello World!!!</div>
+        );
+    }
+}
+
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    const html = renderToString(<HelloWorld />);
+
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>传统的SSR</title>
+        </head>
+        <body>
+            <div id="root">
+                ${html}
+            </div>
+        </body>
+        </html>
+    `)
+}).listen(9001);
